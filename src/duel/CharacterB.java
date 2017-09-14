967f110d39727ce5ef5fd3dba11c30a0c175bb19
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-09-14 15:25:08 -0400

    Character 9/14/17

---
+++ src/duel/CharacterB.java
@@ -0,0 +1,14 @@
+package duel;
+
+public class CharacterB implements Dueler  {
+	public CharacterB(){
+		
+	}
+	public void taunt() {
+		String taunt = "hai";
+		System.out.println(taunt);
+	}
+	public String getname() {
+		return "Namer";
+	}
+}
