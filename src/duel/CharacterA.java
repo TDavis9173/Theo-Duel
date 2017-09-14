73bab19967f1f190c50927238e2f722a958e89b8
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-09-14 15:15:50 -0400

    asdf

---
+++ src/duel/CharacterA.java
@@ -0,0 +1,23 @@
+package duel;
+
+public class CharacterA implements Dueler {
+	public CharacterA() {
+	}
+	public void taunt(){
+		String taunt = "Reeeeeeeeee";
+		System.out.println(taunt);
+	}
+	public String getName(){
+		
+		
+		return "4 chan" ;
+		
+	}
+	public void setStartingHP(int hp) {
+		this.hp = hp;
+	}
+	public int getHP() {
+		
+		return  ;
+	}
+}
