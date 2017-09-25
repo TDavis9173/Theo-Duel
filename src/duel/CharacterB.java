a35b930ed9c90648aa51d1333f540fafe00f39c9
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-09-25 14:54:43 -0400

    Char B

--- src/duel/CharacterB.java
+++ src/duel/CharacterB.java
@@ -1,6 +1,7 @@
 package duel;
 
 public class CharacterB implements Dueler  {
+	public int hitpoints;
 	public CharacterB(){
 		
 	}
@@ -10,5 +11,40 @@ public class CharacterB implements Dueler  {
 	}
 	public String getName() {
 		return "Namer";
+	}
+	public void setStartingHP(int hp) {
+		this.hitpoints = hp;
+		}
+
+	public int getHP() {
+		return this.hitpoints;
+	}
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+		while (d.getHP() == hp) {
+			return true;
+		}
+			return false;
+	}
+	public int getAction(Object caller) {
+		if(caller instanceof Duel) {
+			double rng=Math.random();
+			if(rng<.33) {
+				return 1;
+			}
+			else if(rng>.33 && rng<.66){
+				return 2;
+			}
+			else{
+				return 0;
+			}
+		}
+		else {
+			return 3;
+		}
+	}
+	public void hit(Object caller) {
+		if(caller instanceof Duel) {
+			this.hitpoints-=10;
+		}
 	}
 }
