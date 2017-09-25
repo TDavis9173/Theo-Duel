fedac11476c44d3e98a95e4fb0d6fccd61317573
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-09-25 14:51:35 -0400

    Character A finished

--- src/duel/CharacterA.java
+++ src/duel/CharacterA.java
@@ -22,10 +22,31 @@ public class CharacterA implements Dueler {
 		return this.hp;
 		}
 	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
-		if(d.hp=hp) {
+		if(d.getHP() == hp) {
 			return true;
 		}
-		else
-			return false;
+		return false;
+	}
+	public int getAction(Object caller) {
+		if(caller instanceof Duel) {
+			double rng=Math.random();
+			if(rng<.33) {
+				return 1;
+			}
+			else if(rng>.33 && rng<.66){
+				return 2;
+			}
+			else{
+				return 0;
+			}
+		}
+		else {
+			return 3;
+		}
+	}
+	public void hit(Object caller) {
+		if(caller instanceof Duel) {
+			this.hp-=10;
+		}
 	}
 }
